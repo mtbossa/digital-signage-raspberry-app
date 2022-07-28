201d06d60bc9580455f6127abe50eb1754f8a7cb
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-07-28 09:22:26 -0300

    Add database variables to env

--- apps/raspberry-server/src/mongoose.ts
+++ apps/raspberry-server/src/mongoose.ts
@@ -1,14 +1,29 @@
-import mongoose from 'mongoose';
-import { Application } from './declarations';
-import logger from './logger';
+import mongoose from "mongoose";
+import { Application } from "./declarations";
+import logger from "./logger";
 
 export default function (app: Application): void {
-  mongoose.connect(
-    app.get('mongodb')
-  ).catch(err => {
-    logger.error(err);
-    process.exit(1);
-  });
+	const dbHost = app.get("dbHost");
+	const dbPort = app.get("dbPort");
+	const dbUser = app.get("dbUser");
+	const dbPassword = app.get("dbPassword");
+	const dbName = app.get("dbName");
 
-  app.set('mongooseClient', mongoose);
+	let dbUrl = "mongodb://";
+	if (dbUser && dbPassword) {
+		dbUrl = dbUrl + `${dbUser}:${dbPassword}@`;
+	}
+
+	dbUrl = dbUrl + `${dbHost}:${dbPort}`;
+
+	if (dbName) {
+		dbUrl = dbUrl + `/${dbName}`;
+	}
+
+	mongoose.connect(dbUrl).catch(err => {
+		logger.error(err);
+		process.exit(1);
+	});
+
+	app.set("mongooseClient", mongoose);
 }
