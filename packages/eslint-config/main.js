85fa79f076f5e121dca5d9c7906bdc8004949592
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-08-02 16:26:52 -0300

    Add ignorePatterns

--- packages/eslint-config/main.js
+++ packages/eslint-config/main.js
@@ -5,4 +5,5 @@ module.exports = {
     "simple-import-sort/imports": ["error"],
     "simple-import-sort/exports": ["error"],
   },
+  ignorePatterns: ["**/*.js", "node_modules", ".turbo", "dist", "coverage"],
 };
