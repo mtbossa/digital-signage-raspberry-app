7185584efb15698d8ca83431f6dd1b4c6774685a
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-08-24 17:17:22 -0300

    Run formatting

--- apps/raspberry-server/test/services/posts-sync.test.ts
+++ apps/raspberry-server/test/services/posts-sync.test.ts
@@ -1,8 +1,8 @@
-import app from '../../src/app';
+import app from "../../src/app";
 
-describe('\'posts-sync\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('posts-sync');
+describe("'posts-sync' service", () => {
+  it("registered the service", () => {
+    const service = app.service("posts-sync");
     expect(service).toBeTruthy();
   });
 });
