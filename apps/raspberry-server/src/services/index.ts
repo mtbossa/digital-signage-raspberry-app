28c28d49c18932e7e9be4fbdbe745a88729770bd
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-09-11 10:41:16 -0300

    Move startup logic to service

--- apps/raspberry-server/src/services/index.ts
+++ apps/raspberry-server/src/services/index.ts
@@ -6,6 +6,7 @@ import posts from "./posts/posts.service";
 import postsSync from "./posts-sync/posts-sync.service";
 import serverStatusChecker from "./server-status-checker/server-status-checker.service";
 import showcaseChecker from "./showcase-checker/showcase-checker.service";
+import startup from './startup/startup.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
@@ -16,4 +17,5 @@ export default function (app: Application): void {
   app.configure(showcaseChecker);
   app.configure(backendChannelsConnector);
   app.configure(serverStatusChecker);
+  app.configure(startup);
 }
