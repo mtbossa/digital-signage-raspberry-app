e7f22e0c9d1d41dd1ef39852df2afb8fe395c91e
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-09-11 11:39:59 -0300

    Configuring logger

--- apps/raspberry-server/src/logger.ts
+++ apps/raspberry-server/src/logger.ts
@@ -8,24 +8,35 @@ if (process.env.NODE_ENV === "development") {
     level: "debug",
     format: format.combine(format.splat(), format.simple()),
     transports: [
-      new transports.Console(),
+      new transports.Console({ handleExceptions: true }),
       new transports.File({
         filename: `${logsFolder}/errors.log`,
         level: "error",
       }),
     ],
+    exceptionHandlers: [
+      new transports.File({ filename: `${logsFolder}/exceptions.log` }),
+    ],
+    exitOnError: false,
   });
 } else {
   logger = createLogger({
     level: "info",
     format: format.json(),
     transports: [
-      new transports.Console(),
+      new transports.Console({
+        format: format.combine(format.colorize(), format.simple()),
+        handleExceptions: true,
+      }),
       new transports.File({
         filename: `${logsFolder}/errors.log`,
         level: "error",
       }),
     ],
+    exceptionHandlers: [
+      new transports.File({ filename: `${logsFolder}/exceptions.log` }),
+    ],
+    exitOnError: false,
   });
 }
 
