e58b78b53ed77e88ff8e9e3e5108524e68ffbdf7
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-09-12 19:22:49 -0300

    Fix issue when more then 2 posts

--- apps/raspberry-frontend/src/components/PostShowcase/PostShowcase.tsx
+++ apps/raspberry-frontend/src/components/PostShowcase/PostShowcase.tsx
@@ -25,10 +25,6 @@ export default function PostShowcase({
 }: PostShowcaseProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  // Controls when a post starts playing and stops, so can delete posts when not playing.
-  // Triggers useEffect that will delete posts.
-  const [isPlaying, setIsPlaying] = useState<boolean>(false);
-
   // Current post being showed on the screen
   const [showingPost, setShowingPost] = useState<Post | null>(null);
 
@@ -36,41 +32,42 @@ export default function PostShowcase({
 
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
-    if (!showingPost || Object.keys(showingPost).length === 0) return;
+    if (latestPosts.length === 0) return;
 
-    console.log("showingPost: ", showingPost);
-    console.log("index: ", currentPostIndex);
-
-    setIsPlaying(true);
-    if (showingPost?.media.type === "image") {
-      setTimeout(() => {
-        setIsPlaying(false);
-      }, showingPost.exposeTime);
+    if (!showingPost) {
+      setShowingPost({ ...getNextPost(showingPost) });
     } else {
-      videoRef.current?.play();
-    }
-  }, [showingPost]);
-
-  useEffect(() => {
-    if (!isPlaying) {
-      if (deletablePosts.length > 0) {
-        const postsWithoutDeleted = latestPosts.filter((post) => {
-          return deletablePosts.find(
-            (deletedPost: Pick<Post, "_id">) => post._id !== deletedPost._id
-          );
-        });
-        updatePosts(postsWithoutDeleted);
-        clearDeletablePosts();
+      if (showingPost?.media.type === "image") {
+        setTimeout(() => {
+          handleNextPost();
+        }, showingPost.exposeTime);
+      } else {
+        videoRef.current?.play();
       }
+    }
+  }, [showingPost, latestPosts]);
 
+  function handleOnVideoEnded(event: React.SyntheticEvent<HTMLVideoElement, Event>) {
+    handleNextPost();
+  }
+
+  function handleNextPost() {
+    if (deletablePosts.length > 0) {
+      const postsWithoutDeleted = latestPosts.filter((post) => {
+        return deletablePosts.find(
+          (deletedPost: Pick<Post, "_id">) => post._id !== deletedPost._id
+        );
+      });
+      updatePosts(postsWithoutDeleted);
+      if (postsWithoutDeleted.length === 0) {
+        setShowingPost(null);
+      }
+      clearDeletablePosts();
+    } else {
       if (latestPosts.length > 0) {
         setShowingPost({ ...getNextPost(showingPost) });
       }
     }
-  }, [isPlaying, latestPosts]);
-
-  function handleOnVideoEnded(event: React.SyntheticEvent<HTMLVideoElement, Event>) {
-    setIsPlaying(false);
   }
 
   function getNextPost(post: Post | null): Post {
