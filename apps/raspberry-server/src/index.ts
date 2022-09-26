286e938b5bdc87b79cb2e0a8a662837ac234380b
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-09-26 17:44:28 -0300

    Add logger info

--- apps/raspberry-server/src/index.ts
+++ apps/raspberry-server/src/index.ts
@@ -4,9 +4,12 @@ import logger from "./logger";
 const port = app.get("port");
 const server = app.listen(port);
 
-process.on("unhandledRejection", (reason, p) =>
-  logger.error("Unhandled Rejection at: Promise ", p, reason)
-);
+process.on("unhandledRejection", (reason, p) => {
+  logger.error("Unhandled Rejection at: Promise ", p, reason);
+  if (reason instanceof Error) {
+    logger.error("Message: ", reason.message);
+  }
+});
 
 server.on("listening", async () => {
   logger.info(`Feathers application started on http://${app.get("host")}:${port}`);
