b86160e0865794f887a716092ba6fc2582ab8095
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-09-29 20:14:34 -0300

    Add Loader

--- apps/raspberry-frontend-solidjs/src/App.tsx
+++ apps/raspberry-frontend-solidjs/src/App.tsx
@@ -3,26 +3,14 @@ import client from "./feathers";
 
 import logo from "./logo.svg";
 import styles from "./App.module.css";
+import Loading from "./components/Loading";
 
 const postsService = client.service("posts");
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <Loading />
     </div>
   );
 };
---
+++ apps/raspberry-frontend-solidjs/src/components/Loading/Loading.tsx
@@ -0,0 +1,20 @@
+import styles from "./Loading.module.css";
+
+export default function Loading() {
+  return (
+    <div class={styles["lds-default"]}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
