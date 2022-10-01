4ae1bb90d00d6d88d9a99d768b9da42720461b5f
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-10-01 11:44:15 -0300

    Improve logger

--- apps/raspberry-server/src/logger.ts
+++ apps/raspberry-server/src/logger.ts
@@ -2,14 +2,18 @@ import os from "os";
 import path from "path";
 import { createLogger, format, Logger, transports } from "winston";
 
+type AppEnv = "development" | "staging" | "production";
+const APP_ENV: AppEnv = process.env.NODE_ENV as AppEnv;
+
 let logger: Logger;
 
 const myFormat = format.printf(({ level, message, timestamp }) => {
   return `${timestamp} - ${level}: ${message}`;
 });
 
-if (process.env.NODE_ENV === "development") {
+if (APP_ENV === "development") {
   const logsFolder = "./logs";
+
   logger = createLogger({
     level: "debug",
     format: format.combine(
@@ -34,40 +38,58 @@ if (process.env.NODE_ENV === "development") {
 } else {
   const userHomeDir = os.homedir();
   const logsFolder = path.join(userHomeDir, ".local/", "share/", "intus/logs");
-  logger = createLogger({
-    level: "info",
-    format: format.combine(format.errors({ stack: true }), format.json()),
-    transports: [
-      new transports.Console({
-        format: format.combine(
-          format.colorize(),
-          format.simple(),
-          format.timestamp({ format: "YYYY-MM-DD hh:mm:ss" })
-        ),
-        handleExceptions: true,
-      }),
-      new transports.File({
-        filename: `${logsFolder}/errors.log`,
-        level: "error",
-        format: format.combine(
-          format.errors({ stack: true }),
-          format.timestamp({ format: "YYYY-MM-DD hh:mm:ss" }),
-          format.json()
-        ),
-      }),
-    ],
-    exceptionHandlers: [
-      new transports.File({
-        filename: `${logsFolder}/exceptions.log`,
-        format: format.combine(
-          format.errors({ stack: true }),
-          format.timestamp({ format: "YYYY-MM-DD hh:mm:ss" }),
-          format.json()
-        ),
-      }),
-    ],
-    exitOnError: false,
-  });
+
+  if (APP_ENV === "staging") {
+    logger = createLogger({
+      level: "info",
+      format: format.combine(format.errors({ stack: true }), format.json()),
+      transports: [
+        new transports.Console({
+          format: format.combine(
+            format.colorize(),
+            format.simple(),
+            format.timestamp({ format: "YYYY-MM-DD hh:mm:ss" })
+          ),
+          handleExceptions: true,
+        }),
+        new transports.File({
+          filename: `${logsFolder}/errors.log`,
+          level: "error",
+          format: format.combine(
+            format.errors({ stack: true }),
+            format.timestamp({ format: "YYYY-MM-DD hh:mm:ss" }),
+            format.json()
+          ),
+        }),
+      ],
+      exceptionHandlers: [
+        new transports.File({
+          filename: `${logsFolder}/exceptions.log`,
+          format: format.combine(
+            format.errors({ stack: true }),
+            format.timestamp({ format: "YYYY-MM-DD hh:mm:ss" }),
+            format.json()
+          ),
+        }),
+      ],
+      exitOnError: false,
+    });
+  } else {
+    logger = createLogger({
+      level: "error",
+      format: format.combine(format.errors({ stack: true }), format.json()),
+      transports: [
+        new transports.Console({
+          format: format.combine(
+            format.colorize(),
+            format.simple(),
+            format.timestamp({ format: "YYYY-MM-DD hh:mm:ss" })
+          ),
+          handleExceptions: true,
+        }),
+      ],
+    });
+  }
 }
 
 export default logger;
