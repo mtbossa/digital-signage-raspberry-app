b310e746f5d27215e737b4e2c786a70519ca3566
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-10-29 20:27:03 -0300

    FIx

--- apps/raspberry-server/test/services/display-connect.test.ts
+++
@@ -1,8 +0,0 @@
-import app from "../../src/app";
-
-describe("'display-connect' service", () => {
-  it("registered the service", () => {
-    const service = app.service("display-connect");
-    expect(service).toBeTruthy();
-  });
-});
