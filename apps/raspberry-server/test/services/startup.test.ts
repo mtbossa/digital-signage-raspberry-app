7c8250aab78ba867bb57a58cfcefbefdc8540887
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-10-29 22:13:20 -0300

    Start test

--- apps/raspberry-server/test/services/startup.test.ts
+++ apps/raspberry-server/test/services/startup.test.ts
@@ -1,8 +1,17 @@
 import app from "../../src/app";
 
 describe("'startup' service", () => {
+  afterEach(() => {
+    app.service("server-status-checker").stop();
+    app.service("showcase-checker").stop();
+  });
   it("registered the service", () => {
     const service = app.service("startup");
     expect(service).toBeTruthy();
+  });
+
+  it("test", async () => {
+    const service = app.service("startup");
+    expect(await service.create({})).toEqual({});
   });
 });
