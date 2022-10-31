60d1f162c6b57798fd1f8978de53a7956de0f793
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-10-30 21:41:17 -0300

    Start testing

--- apps/raspberry-server/test/services/startup.test.ts
+++ apps/raspberry-server/test/services/startup.test.ts
@@ -3,19 +3,20 @@ import intusAPI from "../../src/clients/intusAPI/intusAPI";
 
 jest.mock("../../src/clients/intusAPI/intusAPI");
 const mockedIntusAPI = jest.mocked(intusAPI);
+mockedIntusAPI.fetchRaspberryPosts.mockResolvedValue([]);
 
 describe("'startup' service", () => {
   afterEach(() => {
     app.service("server-status-checker").stop();
     app.service("showcase-checker").stop();
   });
+
   it("registered the service", () => {
     const service = app.service("startup");
     expect(service).toBeTruthy();
   });
 
   it("test", async () => {
-    mockedIntusAPI.fetchRaspberryPosts.mockResolvedValue([]);
     const service = app.service("startup");
     expect(await service.create({})).toEqual({});
   });
